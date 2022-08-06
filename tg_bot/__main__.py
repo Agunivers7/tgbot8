4c63dc04a6fdaae8c82b383225fee9365424ff36
Agunivers7 <110654906+Agunivers7@users.noreply.github.com>
2022-08-06 15:09:40 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -29,7 +29,6 @@ PM_START_TEXT = """
 """
 
 HELP_STRINGS = """
-Vanakanda mapula*{}*,Enna help katiya!.
 I'm a modular group management bot with a few fun extras! Have a look at the following for an idea of some of \
 the things I can help you with.
 *Main* commands available:
