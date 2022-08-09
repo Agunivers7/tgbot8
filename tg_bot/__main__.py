7d355d3f8667df4a9cba30193b653dcfdc2ad04e
Agunivers7 <110654906+Agunivers7@users.noreply.github.com>
2022-08-09 15:25:43 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -140,13 +140,29 @@ def start(bot: Bot, update: Update, args: List[str]):
         else:
             first_name = update.effective_user.first_name
             update.effective_message.reply_animation(
-                BOT_IMG, 
-                PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),
+                BOT_IMG,
+                caption=PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(context.bot.first_name), OWNER_ID),
+                parse_mode=ParseMode.MARKDOWN,
+                reply_markup=InlineKeyboardMarkup(
+                    [[
+                        InlineKeyboardButton(
+                            text="➕️Add mizuhara to your group",
+                            url="t.me/{}?startgroup=true".format(context.bot.username))
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            text="Support Chat ",
+                            url="https://t.me/chizurusupport"),
+                        InlineKeyboardButton(
+                            text="📍Updates ",
+                            url="https://t.me/chizuruupdates")
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            text="help",
+                            url="https://t.me/Chizurumanagementbot?start=help")
+                    ]]))
 
-                parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup(
-                    [[InlineKeyboardButton(text=" Add me to your group", url="t.me/{}?startgroup=true".format(bot.username)),  InlineKeyboardButton(text="Creator♥️", url="https://t.me/Spoidermon12")],
-                     [InlineKeyboardButton(text="🥰 Support Group", url="https://t.me/Agunivers_backup"), InlineKeyboardButton(text="🔔Join Channel", url="https://t.me/+-VTuSl1PSf8xODk1")],
-                     [InlineKeyboardButton(text="Source", url="https://github.com/Agunivers/tgbot7"), InlineKeyboardButton(text="Help", url="https://t.me/{}?start=help".format(bot.username)) ]]))
 
     else:
 
